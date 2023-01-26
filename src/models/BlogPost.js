b4be324007881ff552e2b37b91e55398a727f908
Sergio <sergioruza22@gmail.com>
2023-01-26 18:44:26 -0300

    feat: model for user blog_posts

---
+++ src/models/BlogPost.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    underscored: true,
+    tableName: 'blog_posts',
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'users'
+    })
+  }
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -16,5 +16,12 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blogPosts'
+    })
+  }
+
   return User;
 }
