5da2cdcddf1414d09c7ca68e8dd05f615ffee80c
Sergio <sergioruza22@gmail.com>
2023-01-26 16:51:13 -0300

    feat: model for user table

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+    tableName: 'users',
+  });
+
+  return User;
+}
